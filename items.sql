7bba8e7ec145145636cf3745419798a7e085d518
AidwN <86929272+AidwN@users.noreply.github.com>
2021-07-21 04:21:46 +0300

    items

---
+++ items.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('viagra', 'Viagra', 1, 0, 1),
+	('pecete', 'Pecete', 1, 0, 1),
+	('dollupecete', 'Döllü Pecete', 1, 0, 1)
+;
